0901199f75e748568e16f1a0436d94dbf3eda7ea
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2022-06-27 07:26:52 +0300

    Add project files.

---
+++ ParkyAPI/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+
+namespace ParkyAPI
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+
+        }
+        public DbSet<NationalPark> NationalPark { get; set; }
+    }
+}
